43485057a633d907f4fda58e577e2804750f5b5f
Lucy <peachie.monkey@gmail.com>
2014-04-14 04:11:49 -0400

    updated fb.py and added auction.py

---
+++ auction.py
@@ -0,0 +1,44 @@
+import urllib2
+from lxml import etree
+from bs4 import BeautifulSoup
+
+artistmap = {'Banksy':'banksy', 'Andy Warhol':'andy-warhol','Agnes Martin':'agnes-martin', 'Roy Lichtenstein':'roy-lichtenstein','Keith Haring':'keith-haring', 'Pablo Picasso':'pablo-picasso'}
+
+def auction(artist):
+	dates = []
+	low = []
+	high = []
+	final = []
+
+	baseurl = 'http://www.artnet.com/artists/' + artistmap[artist]
+	response = urllib2.urlopen(baseurl+'/recent-auctions')
+	html_doc = response.read() 
+	soup = BeautifulSoup(html_doc)
+	linkdoms = soup.find_all("a", class_="lnk9")
+	links = []
+	for i in xrange(0, len(linkdoms),2):
+		links.append(linkdoms[i]['href'])
+
+	datedoms = soup.find_all("span", class_="font10")
+	for i in range(1, len(datedoms), 2):
+		if datedoms[i].text.find("Sold:") != -1:
+			dates.append(datedoms[i].text[6:])
+
+	print dates
+
+	for i in range(0, len(links)):
+		response = urllib2.urlopen(baseurl+links[i])
+		html_doc = response.read() 
+		soup = BeautifulSoup(html_doc, "lxml")
+		est = soup.find_all(id='lotInformation_tblCellEstimateText')
+		esttokens = est[0].text.split()
+		low.append(int(esttokens[0].replace(",", "")))
+		high.append(int(esttokens[2].replace(",", "")))
+		sold = soup.find_all(id='lotInformation_tblCellSoldForText')
+		soldtokens = sold[0].text.split()
+		final.append(int(soldtokens[0].replace(",", "")))
+
+	print low
+	print high
+	print final
+	return [dates, low, high, final] 
--- fb.py
+++ fb.py
@@ -2,34 +2,48 @@ import urllib2
 import json
 
 dates = ['apr-13', 'may-13', 'jun-13', 'jul-13', 'aug-13', 'sep-13', 'oct-13', 'nov-13', 'dec-13', 'jan-14', 'feb-14', 'mar-14']
+artistmap = {'Banksy':'banksy', 'Andy Warhol':'andywarholpaintings','Agnes Martin':'Agnes-Martin', 'Roy Lichtenstein':'roylichtenstein1','Keith Haring':'Keith-Haring'}
 valuestr = ''
 
-for i in range(0, len(dates)-1):
-	req = urllib2.Request("https://graph.facebook.com/andywarholpaintings/insights?since=1-" +dates[i]+ "&until=2-" +dates[i]+ "&access_token=1388859028003148|7ec5645154cf6a5ea978b5e710f784b0")
-	response = urllib2.urlopen(req)
+# Aggregated Facebook location data, sorted by country, about the people who like your Page
 
-	obj = json.loads(response.read())
-	dat = obj['data'][0]['values'][0]['value']
+def facebook(artist):
+	valuestr = ''
+	likes = []
+	for i in range(0, len(dates)-1):
+		req = urllib2.Request("https://graph.facebook.com/" + artistmap[artist] + "/insights?since=1-" +dates[i]+ "&until=2-" +dates[i]+ "&access_token=1388859028003148|7ec5645154cf6a5ea978b5e710f784b0")
+		response = urllib2.urlopen(req)
 
-	totalfans_0 = 0
+		obj = json.loads(response.read())
+		dat = obj['data'][0]['values'][0]['value']
 
-	for key, value in dat.items():
-		totalfans_0 += value
+		totalfans_0 = 0
 
-	req = urllib2.Request("https://graph.facebook.com/andywarholpaintings/insights?since=1-" +dates[i+1]+ "&until=2-" +dates[i+1]+ "&access_token=1388859028003148|7ec5645154cf6a5ea978b5e710f784b0")
-	response = urllib2.urlopen(req)
+		for key, value in dat.items():
+			totalfans_0 += value
 
-	obj = json.loads(response.read())
-	dat = obj['data'][0]['values'][0]['value']
+		print totalfans_0
 
-	totalfans_1 = 0
+		req = urllib2.Request("https://graph.facebook.com/" + artistmap[artist] + "/insights?since=1-" +dates[i+1]+ "&until=2-" +dates[i+1]+ "&access_token=1388859028003148|7ec5645154cf6a5ea978b5e710f784b0")
+		response = urllib2.urlopen(req)
 
-	for key, value in dat.items():
-		totalfans_1 += value
+		obj = json.loads(response.read())
+		dat = obj['data'][0]['values'][0]['value']
 
-	valuestr += str(totalfans_1-totalfans_0) + ', '
+		totalfans_1 = 0
 
-print valuestr
+		for key, value in dat.items():
+			totalfans_1 += value
+
+		likes.append(int(totalfans_1-totalfans_0));
+		#valuestr += str(totalfans_1-totalfans_0) + ', '
+
+	print likes
+	return likes
+
+
+# The number of people sharing stories about your page ('People Talking About This' / PTAT). These stories include liking your Page, posting to your Page's Wall, liking, commenting on or sharing one of your Page posts, answering a Question you posted, RSVPing to one of your events, mentioning your Page, phototagging your Page or checking in at your Place
+# Note that currently only the weekly value is in real-time if you set until=now in the request. This metric is deprecated after the deprecate_PTAT migration.
 
 valuestr = ''
 for i in range(0, len(dates)):
@@ -46,6 +60,8 @@ for i in range(0, len(dates)):
 
 	valuestr += str(totalfans) + ', '
 
-print valuestr
+print valuestr + ' second'
+
+facebook('Andy Warhol')
 
 #print obj['data'][0]['values'][0]['value']['US']
