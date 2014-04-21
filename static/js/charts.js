3a861fdef655a93b02bb7e3fa510422804ef6916
Lucy <peachie.monkey@gmail.com>
2014-04-21 12:37:36 -0400

    Added auction trend feature
    - also updated glyph icons font files to try and fix broken icons..
    haven't figure this out yet

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
         // console.log(wikix);
         console.log(wikiy);
         $('#wikipedia').highcharts({
@@ -181,6 +182,39 @@ $(document).ready(function() {
 
 });
 
- 
+var p = calculateAuctionStats();
+var rounded_p = (p).toFixed(2);
+
+$('#trend-percent').html(rounded_p);
+
+
+var ex_sale_price;
+if (p < 0){
+    $('#auction-trend').html("On average, recent auctions have sold " + rounded_p + "% of the price range <font color='red'>below</font> the high estimate.");
+    ex_sale_price = 1100 - parseFloat(rounded_p);
+    $('#trend-percent').css("color", "red");
+} else {
+    $('#auction-trend').html("On average, recent auctions have sold " + rounded_p + "% of the price range <font color='green'>above</font> the high estimate.");
+    ex_sale_price = 1100 + parseFloat(rounded_p);    
+    $('#trend-percent').css("color", "green");
+    }
+$('#trend-tooltip').attr("title", "E.g. a piece with a predicted sale price range of $1000 to $1100 would have sold for $" + ex_sale_price);
+$("#trend-tooltip").tooltip();
+    
     });
     
+function calculateAuctionStats(){
+    if (aucx.length < 1){
+        return 0;
+    }
+    var totalpercent = 0;
+    for (var i = 0; i < aucx.length; i++) {
+        var range = auchigh[i]-auclow[i];
+        var sale_diff_from_high = aucfinal[i]-auchigh[i];
+        var percent = sale_diff_from_high/range;
+        totalpercent += percent;
+    } 
+    var avgpercent = totalpercent/aucx.length;
+    return avgpercent*100;
+}
+
